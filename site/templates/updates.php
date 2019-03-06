1c3f0ade5b68803d9ce0238fd5c34c204094818e
Paul Gomez <pauldro952@outlook.com>
2019-03-06 11:43:51 -0600

    Item Label Print
    Merged from cptechinc/soft-dpluso@c6430bfbfa8ee5b734cbb52dae8a10b6bb410ecd

--- site/templates/updates.php
+++ site/templates/updates.php
@@ -1,36 +1,37 @@
-<?php 
+<?php
     $template = 'warehouse-function';
     $binrpage = $pages->get('/warehouse/binr/');
     $binrpage->of(false);
-    $binrpage->dplusfunction = 'binr';
-    
+    $binrpage->dplusfunction = 'mbinr';
+	$binrpage->save();
+	
     $binr_pages = array(
         'binr' => array(
             'name' => 'binr',
             'title' => 'Bin Reassignment',
             'summary' => 'Move Bin Item',
-            'function' => 'binr'
+            'function' => 'pbinr'
         ),
         'move-from' => array(
             'name' => 'move-from',
             'title' => 'Move From',
             'summary' => 'Move Single items from same from bin',
-            'function' => 'binr'
+            'function' => 'pbnrf'
         ),
         'move-to' => array(
             'name' => 'move-to',
             'title' => 'Move To',
             'summary' => 'Move Single Items to the same to bin',
-            'function' => 'binr'
+            'function' => 'pbnrt'
         ),
         'move-bin-contents' => array(
             'name' => 'move-bin-contents',
             'title' => 'Move Bin Contents',
             'summary' => 'Move all Bin Items',
-            'function' => 'binr'
+            'function' => 'pmvbn'
         )
     );
-    
+
     foreach ($binr_pages as $pagename => $page) {
         if (!$binrpage->numChildren("name=$pagename")) {
             $p = new Page();
@@ -50,11 +51,12 @@
             $p->save();
         }
     }
-    
+
     $inventorypage = $pages->get('/warehouse/inventory/');
     $inventorypage->of(false);
-    $inventorypage->dplusfunction = 'wm';
-    
+    $inventorypage->dplusfunction = 'mivty';
+	$inventorypage->save();
+
     $inventory_pages = array(
         'physical-count' => array(
             'name' => 'physical-count',
@@ -66,16 +68,22 @@
             'name' => 'find-item',
             'title' => 'Find Item',
             'summary' => 'Find Location(s) for an Item',
-            'function' => 'wm'
+            'function' => 'pfini'
         ),
         'bin-inquiry' => array(
             'name' => 'bin-inquiry',
             'title' => 'Bin Inquiry',
             'summary' => 'Show Items that are in a bin',
+            'function' => 'pbini'
+        ),
+		'print-item-label' => array(
+			'name' => 'print-item-label',
+            'title' => 'Print Item Label',
+            'summary' => 'Print a Label for a bin Item',
             'function' => 'wm'
-        )
+		)
     );
-    
+
     foreach ($inventory_pages as $pagename => $page) {
         if ($inventorypage->numChildren("name=$pagename")) {
             $p = $inventorypage->child("name=$pagename");
@@ -86,26 +94,27 @@
             $p->save();
         }
     }
-    
+
     $pickingpage = $pages->get('/warehouse/picking/');
     $pickingpage->of(false);
-    $pickingpage->dplusfunction = 'wm';
-    
+    $pickingpage->dplusfunction = 'mppik';
+	$pickingpage->save();
+
     $picking_pages = array(
         'pick-order' => array(
             'name' => 'pick-order',
             'title' => 'Sales Order Picking',
             'summary' => 'Pick Items for a Sales Order',
-            'function' => 'wm'
+            'function' => 'porpk'
         ),
         'pick-pack' => array(
             'name' => 'pick-pack',
             'title' => 'Sales Order Picking & Packing',
             'summary' => 'Pick & Pack Items for a Sales Order',
-            'function' => 'wm'
+            'function' => 'popp'
         )
     );
-    
+
     foreach ($picking_pages as $pagename => $page) {
         if ($pickingpage->numChildren("name=$pagename")) {
             $p = $pickingpage->child("name=$pagename");
@@ -116,8 +125,9 @@
             $p->save();
         }
     }
-    
+
     $warehousepage = $pages->get('/warehouse/');
     $warehousepage->of(false);
-    $warehousepage->dplusfunction = 'wm';
+    $warehousepage->dplusfunction = 'mpwm';
+	$warehousepage->save();
 ?>
